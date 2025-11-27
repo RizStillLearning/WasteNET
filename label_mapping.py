85e60b06c8fc532bb0d8be0af33109b9bae2959f
harris_krs26 <harrisk2606@gmail.com>
2025-11-27 20:11:31 +0700

    Added label_mapping.py

---
+++ label_mapping.py
@@ -0,0 +1,14 @@
+index_to_label = {
+    0: 'battery',
+    1: 'biological',
+    2: 'cardboard',
+    3: 'clothes',
+    4: 'glass',
+    5: 'metal',
+    6: 'paper',
+    7: 'plastic',
+    8: 'shoes',
+    9: 'trash',
+}
+
+label_to_index = {v: k for k, v in index_to_label.items()}
